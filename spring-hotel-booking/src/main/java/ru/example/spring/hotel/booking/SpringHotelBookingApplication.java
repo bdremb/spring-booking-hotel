f4844a63dacfa6da934f964920551c5291f29e60
Denis Bobkin <64804176+bdremb@users.noreply.github.com>
2024-07-23 10:14:48 +0300

    init

---
+++ spring-hotel-booking/src/main/java/ru/example/spring/hotel/booking/SpringHotelBookingApplication.java
@@ -0,0 +1,13 @@
+package ru.example.spring.hotel.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHotelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHotelBookingApplication.class, args);
+    }
+
+}
